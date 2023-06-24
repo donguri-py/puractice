5047f98abfdc86d308c38bf4201066c34565bc06
donguri-py <turntabroz@gmail.com>
2023-06-24 20:07:55 +0900

    Docstrings

---
+++ 0624_3.py
@@ -0,0 +1,13 @@
+def test_func(num1, num2):
+    """
+    :param num1: (int): The first number.
+    :param num2: (str): The second number.
+
+    :return:
+        bool: The return value. True for success, False otherwise.
+
+    """
+    print(num1)
+    print(num2)
+    return True
+print(test_func.__doc__)
