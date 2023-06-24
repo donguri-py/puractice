7af2ea4aa26c1eed2281c537c7f37198d92e9f67
donguri-py <turntabroz@gmail.com>
2023-06-24 15:56:47 +0900

    改行する方法

---
+++ 0624_2.py
@@ -0,0 +1,11 @@
+# 80文字を超える場合は改行する
+
+# () と + で書くパターン
+n = ("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+     + "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
+print(n)
+
+# \ と + で書くパターン
+x = 1 + 2 + 3 + 4 + 5 + 6 + 7 \
+    + 8 + 9 + 10
+print(x)
