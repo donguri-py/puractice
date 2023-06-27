cafe6b5d3c6f177c3aa2048185dd3b9bf25029df
donguri-py <turntabroz@gmail.com>
2023-06-28 07:35:38 +0900

    repeat関数

--- 0628_3.py
+++ 0628_3.py
@@ -6,3 +6,7 @@ z = [[(i, j) for j in range(3)] for i in range(5)]
 print(x)
 print(y)
 print(z)
+
+a = [[(i, j) for j in range(1, 5)] for i in range(6, 11)]
+print(a)
+
---
+++ 0628_4.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x = 1
+y = [1, 2, 3]
+z = [[1, 2],[3, 4]]
+
+print(np.repeat(x,5))
+print(np.repeat(y, 3))
+print(np.repeat(z,2))
+print(np.repeat(z, 10, 1))
